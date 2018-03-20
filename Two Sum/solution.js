89644b719e1e00f69fe2dacf0e3cc0778b57de4a
tinywall <1051261239@qq.com>
2018-03-20 11:31:04 +0800

    complete "two sum"

---
+++ Two Sum/solution.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let length = nums.length;
+    for (let i = 0; i < length; i++) {
+        for (let j = 0; j < length; j++) {
+            if (nums[j] == (target - nums[i])) {
+                return [i, j];
+            }
+        }
+    }
+    return [];
+}
---
+++ Two Sum/update-solution.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let res = {};
+    for (let i = 0; i < nums.length; i++) {
+        let tmp = target - nums[i];
+        if (res[tmp] !== undefined) {
+            return [res[tmp], i];
+        }
+        res[nums[i]] = i;
+    }
+    return [];
+};
